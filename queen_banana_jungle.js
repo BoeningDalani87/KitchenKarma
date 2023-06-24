8ded55f09d86fb48b4b4a668d76f78d5d489fe38
BoeningDalani87 <137491794+BoeningDalani87@users.noreply.github.com>
2023-06-25 06:41:26 +0700

    Update API endpoint validations

---
+++ queen_banana_jungle.js
@@ -0,0 +1,90 @@
+//File 1
+//Main File 
+
+//Variables 
+let compostableWaste;
+let compostBins;
+let wasteCollectionDay;
+let compostServiceSubscription; 
+
+//Functions
+function assignCompostableWaste(){
+	//Compostable waste is assigned to each household 
+	compostableWaste = 'fruit, vegetable peels, egg shells, coffee grounds and filters, tea bags, paper towels, and yard trimmings';
+};
+function assignCompostBins(){
+	//Compost bins are assigned to each household  
+	compostBins = 'kitchen-scrap bin, food-scrap bin, and/or yard-waste bin';
+};
+function assignWasteCollectionDay(){
+	//Waste collection day is assigned to each household 
+	wasteCollectionDay = 'weekly';
+};
+function assignCompostServiceSubscription(){
+	//Compost service subscription is assigned to each household 
+	compostServiceSubscription = 'monthly or yearly';
+};
+
+//Main Program 
+assignCompostableWaste();
+assignCompostBins();
+assignWasteCollectionDay();
+assignCompostServiceSubscription();
+
+//File 2 
+
+//Variables 
+let pickupSchedule;
+let compostPickupDay;
+let compostCollectionCrew;
+let compostCollectionTruck;
+
+//Functions 
+function assignPickupSchedule(){
+	//Pickup schedule is assigned to each household 
+	pickupSchedule = 'weekly or bi-weekly';
+};
+function assignCompostPickupDay(){
+	//Compost pickup day is assigned to each household 
+	compostPickupDay = 'Monday, Wednesday or Friday';
+};
+function assignCompostCollectionCrew(){
+	//Compost collection crew is assigned to each household 
+	compostCollectionCrew = 'trained and experienced';
+};
+function assignCompostCollectionTruck(){
+	//Compost collection truck is assigned to each household 
+	compostCollectionTruck = 'specialized compost collection vehicle';
+};
+
+//Main Program 
+assignPickupSchedule();
+assignCompostPickupDay();
+assignCompostCollectionCrew();
+assignCompostCollectionTruck();
+
+//File 3 
+
+//Variables 
+let compostingProcess;
+let compostDestination;
+let serviceReport;
+
+//Functions 
+function assignCompostingProcess(){
+	//Composting process is assigned to each household 
+	compostingProcess = 'aerobic digestion, which is the breakdown of organic material by bacteria and other organisms in the presence of oxygen';
+};
+function assignCompostDestination(){
+	//Compost destination is assigned to each household 
+	compostDestination = 'local farms, gardens, parks and other green spaces';
+};
+function assignServiceReport(){
+	//Service report is assigned to each household 
+	serviceReport = 'monthly, quarterly, or annual';
+};
+
+//Main Program 
+assignCompostingProcess();
+assignCompostDestination();
+assignServiceReport();
